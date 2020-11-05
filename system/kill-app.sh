c2d6d3647aa35558cb65e385dcad9b9b6e4b6c14
Jimmy McDermott <mcdappdev@gmail.com>
2020-11-05 08:46:16 -0500

    Kill Run app

---
+++ system/kill-app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Kill Run
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.description Kill the running Vapor app
+# @raycast.packageName Vapor
+
+killall Run
