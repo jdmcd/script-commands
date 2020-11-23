044afb4dace0e780e51661a34a1a56fc101c1fa9
Jimmy McDermott <mcdappdev@gmail.com>
2020-11-22 19:14:38 -0500

    Conflicts

--- commands/developer-utils/whois.sh
+++ commands/developer-utils/whois.sh
@@ -2,11 +2,7 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-<<<<<<< HEAD
-# @raycast.title Whois of Domain
-=======
 # @raycast.title Whois
->>>>>>> 3019d39997daf4264db2813bfb9cb9f80fd4cf40
 # @raycast.mode silent
 
 # Optional parameters:
@@ -15,12 +11,6 @@
 # @raycast.description Whois of URL.
 # @raycast.packageName Internet
 # @raycast.icon 🌐
-<<<<<<< HEAD
-# @raycast.argument1 { "type": "text", "placeholder": "domain" }
-
-open "https://who.is/whois/${1}"
-=======
 # @raycast.argument1 { "type": "text", "placeholder": "URL" }
 
 open https://who.is/whois/$1
->>>>>>> 3019d39997daf4264db2813bfb9cb9f80fd4cf40
